916822ca101a0b355e3497b1e2309b29699bf695
unknown <nathansilvacosta2303@gmail.com>
2025-08-24 19:34:05 -0300

    JavaScript configurado para pegar as informações do arquivo JSON e criar os cards de forma dinamica

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
 'use strict'
 
 import jogos from './jogos.json' with {type: "json"}
+
+function criarCard(jogo) {
+    const container = document.getElementById('container')
+
+    const card = document.createElement('div')
+    const gameInfo = document.createElement('div')
+    const title = document.createElement('p')
+    const year = document.createElement('p')
+    const genre = document.createElement('p')
+    const platforms = document.createElement('p')
+    const score = document.createElement('p')
+    const description = document.createElement('p')
+    const img = document.createElement('img')
+
+    card.className = 'card'
+    gameInfo.className = 'game-info'
+    title.className = 'game-title'
+    img.className = 'game-img'
+
+    title.textContent = jogo.title
+    year.textContent = 'Year: ' + jogo.year
+    genre.textContent = 'Genres: ' + jogo.genre
+    platforms.textContent = 'Platforms: ' + jogo.platforms
+    score.textContent = 'Score: ' + jogo.score
+    description.textContent = 'Description: ' + jogo.shortDescription
+    img.src = jogo.imgUrl
+
+    gameInfo.appendChild(title)
+    gameInfo.appendChild(year)
+    gameInfo.appendChild(genre)
+    gameInfo.appendChild(platforms)
+    gameInfo.appendChild(score)
+    gameInfo.appendChild(description)
+
+    card.appendChild(gameInfo)
+    card.appendChild(img)
+
+    container.appendChild(card)
+}
+
+jogos.forEach(criarCard)
