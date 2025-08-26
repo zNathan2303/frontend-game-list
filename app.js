f7f96d5b0234b4bcad896404df04cccd00724266
unknown <nathansilvacosta2303@gmail.com>
2025-08-25 22:00:56 -0300

    Implementação do Axios para consumir a API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import jogos from './jogos.json' with {type: "json"}
+const PLACEHOLDER_GAME_IMG = './img/placeholder_game_error_225x300.jpg'
 
 function criarCard(jogo) {
     const container = document.getElementById('container')
@@ -9,9 +9,6 @@ function criarCard(jogo) {
     const gameInfo = document.createElement('div')
     const title = document.createElement('p')
     const year = document.createElement('p')
-    const genre = document.createElement('p')
-    const platforms = document.createElement('p')
-    const score = document.createElement('p')
     const description = document.createElement('p')
     const img = document.createElement('img')
 
@@ -20,19 +17,26 @@ function criarCard(jogo) {
     title.className = 'game-title'
     img.className = 'game-img'
 
-    title.textContent = jogo.title
-    year.textContent = 'Year: ' + jogo.year
-    genre.textContent = 'Genres: ' + jogo.genre
-    platforms.textContent = 'Platforms: ' + jogo.platforms
-    score.textContent = 'Score: ' + jogo.score
-    description.textContent = 'Description: ' + jogo.shortDescription
-    img.src = jogo.imgUrl
+    title.textContent = jogo.title || 'N/A'
+    year.textContent = `Year: ${jogo.year || 'N/A'}`
+    description.textContent = `Description: ${jogo.shortDescription || 'N/A'}`
+    img.src = jogo.imgUrl || PLACEHOLDER_GAME_IMG
 
-    gameInfo.append(title, year, genre, platforms, score, description)
+    gameInfo.append(title, year, description)
 
     card.append(gameInfo, img)
 
     container.append(card)
 }
 
-jogos.forEach(criarCard)
+function mostrarJogos(jogos) {
+    jogos.forEach(criarCard)
+}
+
+axios.get('http://localhost:8080/games')
+    .then((response) => {
+        mostrarJogos(response.data)
+    })
+    .catch((error) => {
+        console.log('Ocorreu um erro ao carregar os jogos: ' + error)
+    })
