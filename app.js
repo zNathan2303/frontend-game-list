523600936cf94630825560c43a0b8aa46a95c589
unknown <nathansilvacosta2303@gmail.com>
2025-08-26 21:16:11 -0300

    Configurado o modal da página que mostrará mais informações sobre o jogo que foi clicado pelo usuário além do consumo do endpoint da API que utiliza o ID do jogo para retornar todas as informações do mesmo

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 import jogos from './jogos.json' with {type: "json"}
 
 const PLACEHOLDER_GAME_IMG = './img/placeholder_game_error_225x300.jpg'
+const URL_API = 'http://localhost:8080'
 
-function criarCard(jogo) {
+function criarCards(jogo) {
     const container = document.getElementById('container')
 
     const card = document.createElement('div')
@@ -12,30 +13,75 @@ function criarCard(jogo) {
     const title = document.createElement('p')
     const year = document.createElement('p')
     const description = document.createElement('p')
-    const img = document.createElement('img')
+    const imagem = document.createElement('img')
 
     card.className = 'card'
     gameInfo.className = 'game-info'
     title.className = 'game-title'
-    img.className = 'game-img'
+    imagem.className = 'game-img'
 
     title.textContent = jogo.title || 'N/A'
     year.textContent = `Year: ${jogo.year || 'N/A'}`
     description.textContent = `Description: ${jogo.shortDescription || 'N/A'}`
-    img.src = jogo.imgUrl || PLACEHOLDER_GAME_IMG
+    imagem.src = jogo.imgUrl || PLACEHOLDER_GAME_IMG
 
     gameInfo.append(title, year, description)
 
-    card.append(gameInfo, img)
+    card.append(gameInfo, imagem)
+
+    card.dataset.gameId = jogo.id
+
+    card.addEventListener('click', () => {
+        const id = card.dataset.gameId
+        axios.get(URL_API + '/games/' + id)
+            .then((response) => {
+                mostrarModal(response.data)
+                console.log('ID PEGO COM SUCESSO')
+            })
+            .catch((error) => {
+                console.log('ERRO AO PEGAR ID')
+            })
+        mostrarModal(jogo)
+    })
 
     container.append(card)
 }
 
-function mostrarJogos(jogos) {
-    jogos.forEach(criarCard)
+function mostrarModal(jogo) {
+    const modal = document.getElementById('modal-game')
+    const title = document.getElementById('modal-game-title')
+    const year = document.getElementById('modal-year')
+    const genres = document.getElementById('modal-genres')
+    const platforms = document.getElementById('modal-platforms')
+    const score = document.getElementById('modal-score')
+    const description = document.getElementById('modal-description')
+    const imagem = document.getElementById('modal-game-img')
+
+    modal.classList.add('modal-habilitado')
+    modal.classList.remove('modal-desabilitado')
+
+    title.textContent = jogo.title
+    year.textContent = `Year: ${jogo.year || 'N/A'}`
+    genres.textContent = `Genres: ${jogo.genre || 'N/A'}`
+    platforms.textContent = `Platforms: ${jogo.platforms || 'N/A'}`
+    score.textContent = `Platforms: ${jogo.score || 'N/A'}`
+    description.textContent = `Description: ${jogo.longDescription || 'N/A'}`
+    imagem.src = jogo.imgUrl || PLACEHOLDER_GAME_IMG
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.classList.remove('modal-habilitado')
+            modal.classList.add('modal-desabilitado')
+        }
+    }
+
 }
 
-axios.get('http://localhost:8080/games')
+function mostrarJogos(jogos) {
+    jogos.forEach(criarCards)
+}
+
+axios.get(URL_API + '/games')
     .then((response) => {
         mostrarJogos(response.data)
         console.log('Os jogos exibidos são do banco de dados!')
